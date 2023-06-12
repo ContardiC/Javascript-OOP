0d164c9c446c404339e6fd9cf9f2f8e2ffb93647
Carlo Contardi <c.contardi@icloud.com>
2023-06-12 13:25:56 +0200

    app.js updated

--- app.js
+++ app.js
@@ -6,6 +6,32 @@ let dog = {
   }
 
 };
+
 console.log(dog.name, dog.numLegs);
 
 console.log(dog.sayLegs());
+
+/*
+best way to use object attributes.
+In case the name of the object changes the method will work without problems.
+
+-> Use this to refer to the object <-
+*/
+
+let dog2 = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog2.sayLegs();
+
+// constructor
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+
+let hound = new Dog();
