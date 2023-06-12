f560ca3bfcd3691b1b562325a9824c54ff87d08b
Carlo Contardi <c.contardi@icloud.com>
2023-06-12 13:47:58 +0200

    app.js updated

--- app.js
+++ app.js
@@ -88,19 +88,47 @@ console.log(beagle.numLegs);
 
 function Dog(name) {
     this.name = name;
-  }
-  
-  Dog.prototype.numLegs = 4;
-  
-  let beagle = new Dog("Snoopy");
-  
-  let ownProps = [];
-  let prototypeProps = [];
-  
-  for(let property in beagle){
-    if(beagle.hasOwnProperty(property)){
-      ownProps.push(property);
-    }else{
-      prototypeProps.push(property);
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+    if (beagle.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
     }
-  }
+}
+
+// check the constructor
+
+function Dog(name) {
+    this.name = name;
+}
+
+function joinDogFraternity(candidate) {
+    return candidate.constructor === Dog;
+}
+
+// Change the prototype into a new object
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    numLegs: 4,
+    eat: function () {
+        console.log("GNAM GNAM GNAM");
+    },
+    describe: function () {
+        console.log("My name is " + this.name);
+    }
+
+
+};
